13fb8a01a7890e2850b11a9447e6756f67ed4b44
Marci <Marci@Marci-PC>
2012-04-02 19:05:34 +0200

    Added a comment to TestActivity.java

--- test/src/fabo/first/TestActivity.java
+++ test/src/fabo/first/TestActivity.java
@@ -2,7 +2,7 @@ package fabo.first;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+//this is a comment
 public class TestActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
